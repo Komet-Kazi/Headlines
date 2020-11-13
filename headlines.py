0ea6e30f277a2a86817f334e65d93de7cd278508
JD <levitski.jd@outlook.com>
2020-11-13 12:39:26 -0700

    added logic under the get_news function to read RSS from BBC and display a html page with information from the RSS feed. Works as expected.

--- headlines.py
+++ headlines.py
@@ -1,10 +1,23 @@
+import feedparser
 from flask import Flask
+
 
 app = Flask(__name__)
 
+BBC_FEED = 'http://feeds.bbci.co.uk/news/rss.xml'
+
 @app.route('/')
 def get_news():
-    return 'No news is good news'
-
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return '''<html>
+    <body>
+    <h1> BBC Headlines </h1>
+    <b>{0}</b> <br/>
+    <i>{1}</i> <br/>
+    <p>{2}</p> <br/>
+    </body>
+    </html>'''.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))  # we used the .get() operator instead of using index notation (square brackets)
+    # meaning that if any information is missing, it'll simply be omitted from our final HTML rather than causing a runtime error.
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
