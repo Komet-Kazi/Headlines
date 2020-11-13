a384026062c92e271b266ab8693c0c39ec3f3a84
JD <levitski.jd@outlook.com>
2020-11-13 12:50:23 -0700

    restructured to retrieve the rss feed of 4 predefined sites based on the endpoint of the url

--- headlines.py
+++ headlines.py
@@ -4,11 +4,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = 'http://feeds.bbci.co.uk/news/rss.xml'
+# Dictionary of Rss urls and Publishers
+RSS_FEEDS = {
+    'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition.rss',
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'iol': 'http://www.iol.co.za/cmlink/1.640'
+}
+
+# --------------------------------------------------------------------------------------- #
+# routing different predefined endpoints to get news from different publishers
 
 @app.route('/')
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route('/bbc')
+def bbc():
+    return get_news('bbc')
+
+@app.route('/cnn')
+def cnn():
+    return get_news('cnn')
+
+@app.route('/fox')
+def fox():
+    return get_news('fox')
+
+@app.route('/iol')
+def iol():
+    return get_news('iol')
+# --------------------------------------------------------------------------------------- #
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return '''<html>
     <body>
@@ -17,7 +44,9 @@ def get_news():
     <i>{1}</i> <br/>
     <p>{2}</p> <br/>
     </body>
-    </html>'''.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))  # we used the .get() operator instead of using index notation (square brackets)
+    </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+    # we used the .get() operator instead of using index notation (square brackets)
     # meaning that if any information is missing, it'll simply be omitted from our final HTML rather than causing a runtime error.
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
