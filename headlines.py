504ade4ddde1040a5628719eaa99a74a3359f27c
JD <levitski.jd@outlook.com>
2020-11-14 21:19:10 -0700

    added logging configurations and some comments

--- headlines.py
+++ headlines.py
@@ -6,6 +6,26 @@ import json
 import requests
 import urllib.parse
 
+# --------------------------------------------------------------------------------------- #
+# logging configuration
+
+import http
+import logging
+from pathlib import Path
+
+# The path to the directory where this file is saved
+CURRENT_DIRECTORY = Path(__file__).parent
+
+# The path/file to send logs too (so file extension is .log not .py.log)
+LOG_LOCATION = CURRENT_DIRECTORY.joinpath(f'{Path(__file__).stem}.log')
+
+# Configure logging to file
+logging.basicConfig(level='DEBUG', filename=LOG_LOCATION, filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+# Changing the logging debug level greater than 0 will log the response HTTP headers to stdout.
+# useful if you're dealing with an API that returns a large body payload that is not suitable for logging or contains binary content.
+http.client.HTTPConnection.debuglevel = 1
+# --------------------------------------------------------------------------------------- #
 
 app = Flask(__name__)
 
@@ -44,15 +64,21 @@ def get_weather(query):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={query}&units=metric&appid=01b39def802d744725886f7476405653'
     data = requests.get(url)
     parsed = data.json()
+    logging.debug(jPrint(parsed))
     weather = None
     if parsed.get('weather'):
         weather = {'description':
         parsed['weather'][0]['description'],
         'temperature': parsed['main']['temp'],
-        'city': parsed['name']
-        }
+        'city': parsed['name']}
+
     return weather
 
+def jPrint(obj):
+    '''Format a json object into a formatted string
+    Returns: formated string
+    '''
+    return json.dumps(obj, sort_keys=True, indent=4)
 
     uel =api
 if __name__ == "__main__":
